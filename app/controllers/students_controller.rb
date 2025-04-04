41293d973667d2772d976a788a632fd5f60c7120
sbuker <sbuker@protel.com.tr>
2025-04-04 18:32:56 +0300

    Add student model and views

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,24 @@
 class StudentsController < ApplicationController
   def index
+    @students = Student.all
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to students_path, notice: "Öğrenci eklendi"
+    else
+      render :new
+    end
   end
 
-  def show
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :student_number, :photo)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get "students/index"
   get "students/new"
   get "students/create"
   get "students/show"
+
+  resources :students
+  root "students#index"
 
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
